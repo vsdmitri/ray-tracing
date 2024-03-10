bde3a58be91f7f9dccc3fba0df230383a1818fb4
Dmitri Vasilev <lait1103@mail.ru>
2024-03-11 01:33:57 +0300

    rewrite distribution p_factor

--- src/Distributions/Distribution.cpp
+++ src/Distributions/Distribution.cpp
@@ -1,8 +1,8 @@
 #include "Distribution.h"
 
 double Distribution::get_p_factor(const glm::dvec3& x, const glm::dvec3& y, const glm::dvec3& n) const {
-    auto w = x - y;
-    auto dist = glm::length(w);
+    auto w = glm::normalize(x - y);
+    auto dist = glm::length(x - y);
     auto cos = std::abs(glm::dot(n, w));
-    return dist * dist * dist / cos;
+    return dist * dist / cos;
 }
