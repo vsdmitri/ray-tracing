29e182a1daeb1e8e8ac44f807974f50d1c6d8b04
Dmitri Vasilev <lait1103@mail.ru>
2024-03-10 23:49:27 +0300

    abs -> std::abs:

--- src/Distributions/Distribution.cpp
+++ src/Distributions/Distribution.cpp
@@ -3,6 +3,6 @@
 double Distribution::get_p_factor(const glm::dvec3& x, const glm::dvec3& y, const glm::dvec3& n) const {
     auto w = x - y;
     auto dist = glm::length(w);
-    auto cos = abs(glm::dot(n, w));
+    auto cos = std::abs(glm::dot(n, w));
     return dist * dist * dist / cos;
 }
